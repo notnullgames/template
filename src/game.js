e0638bf8de80624a50a0611c2161d811c6bb831f
David Konsumer <konsumer@jetboystudio.com>
2014-12-17 15:08:13 -0800

    working more on resizing

--- src/game.js
+++ src/game.js
@@ -1,6 +1,6 @@
 var Phaser = require('phaser');
 
-var game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.WEBGL, 'phaser-example', { preload: preload, create: create, update: update, render: render });
+var game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update: update, render: render });
 
 function preload() {
   game.load.image('flyer', 'images/logo.png');
@@ -9,13 +9,17 @@ function preload() {
 var image;
 
 function create() {
-  game.renderer.autoResize = true;
+  game.stage.backgroundColor = '#212121';
   game.physics.startSystem(Phaser.Physics.ARCADE);
   image = game.add.sprite(0, 0, 'flyer');
   game.physics.enable(image, Phaser.Physics.ARCADE);
   image.body.velocity.setTo(200,200);
   image.body.collideWorldBounds = true;
   image.body.bounce.set(1);
+
+  window.addEventListener('resize', function(){
+    game.renderer.resize(window.innerWidth, window.innerHeight);
+  });
 }
 
 function update () {
